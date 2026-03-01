7d3394b41ea7249f679f3b303417c19a21edf49c
Andrija Feher <afeher@synapticon.com>
2026-03-01 22:33:16 +0800

    Replace fraction-crusher.html with index.html in ASSETS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'fc-v1';
 const ASSETS = [
   './',
-  './fraction-crusher.html',
+  './index.html',
   './icon-192.png',
   './icon-512.png',
   'https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700;800;900&family=Share+Tech+Mono&family=Rajdhani:wght@500;600;700&display=swap'
